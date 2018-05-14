bf90fe090b84bd845cb574b2148b7c575e26ff64
Meridani <szmanndani@gmail.com>
2018-05-14 23:00:56 +0200

    travels initial

---
+++ travels.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html">
+
+<head>
+    <?php include "head.html"; ?>
+    <title>Utazások - GPS nyomkövetés</title>
+</head>
+
+<body>
+
+<?php include "header.html"; ?>
+<!-- Start of Body -->
+<div class="container main-content">
+    <div class="jumbotron">
+        <h1>Utazások</h1>
+    </div>
+</div>
+
+<?php include "footer.html"; ?>
+
+<!-- End of Body -->
+</body>
+</html>
